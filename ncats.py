4fc2b41448f2550ffbe1d548d649916ff94b3b80
notaisha <aishaajanahi@gmail.com>
2024-05-28 00:04:30 -0400

    heatmap in app

--- ncats.py
+++ ncats.py
@@ -6,13 +6,6 @@ import plotly.express as px
 
 "### Hello, Alec! <3 :bee:"
 
-'''
-- do you want the names actually changed for each file? or do you just want the results or whatever calsulation we come up with to be clear?
-- would you rather type a path to all your documents? or drag and drop?
-- will your csv always be a csv, and will it always have the same headers?
-- will the csv and the stat files always be saved within the same structure of folders? (this doesn't matter if you prefer to drag and drop and don't want names changed)
-- will the stat files always start the same way, with 6 lines to be ignored
-'''
 
 file = st.file_uploader("upload file here")
 if not file:
@@ -33,12 +26,13 @@ stat_file_name_dict = {}
 for stat_file in stat_files:
     for read in df.index:
         if read in stat_file.name:
-            stat_file.name, read
             stat_file_name_dict[stat_file.name]=[read, dict["Compound"][read],dict["Cell line"][read]]
+            st.write('\t'.join(stat_file_name_dict[stat_file.name]))
             df_stat = pd.read_csv(stat_file,delimiter='\t', skiprows=6, nrows=32, index_col=0)
             df_stat
             fig = px.imshow(df_stat)
-            fig.show()
+            #fig.show()
+            st.plotly_chart(fig)
 
 
 stat_file_name_dict
