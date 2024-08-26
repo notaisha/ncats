0fb19f8f1d3a456240707fe530e31d4a137e0015
notaisha <aishaajanahi@gmail.com>
2024-08-26 14:40:09 -0400

    remove table

--- ncats.py
+++ ncats.py
@@ -21,15 +21,16 @@ dict = df.to_dict()
 stat_files = st.file_uploader("drag all stat files here", accept_multiple_files=True)
 if not stat_files:
     st.stop()
-
+show_table = st.checkbox("Show table")
 stat_file_name_dict = {}
 for stat_file in stat_files:
     for read in df.index:
         if read in stat_file.name:
             stat_file_name_dict[stat_file.name]=[read, dict["Compound"][read],dict["Cell line"][read]]
-            st.write('\t'.join(stat_file_name_dict[stat_file.name]))
+            t.write('\t'.join(stat_file_name_dict[stat_file.name]))
             df_stat = pd.read_csv(stat_file,delimiter='\t', skiprows=6, nrows=32, index_col=0)
-            df_stat
+            if show_table:
+                df_stat
             fig = px.imshow(df_stat)
             #fig.show()
             st.plotly_chart(fig)
