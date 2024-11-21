4b75d188b14d2aa172c6304631f3a069756b3ea3
notaisha <aishaajanahi@gmail.com>
2024-11-20 22:51:21 -0500

    add offline runs

--- ncats.py
+++ ncats.py
@@ -8,43 +8,49 @@ import plotly.graph_objects as go
 
 "### Hello, Alec! <3 :bee:"
 
-offline = st.toggle("Robot run", value=True)
+online = st.toggle("Robot run", value=True)
 
-#if offline:
+if online:
 
+    file = st.file_uploader("Upload the CSV file that has the mandatory columns [Read] and [Compound], and an optional [Cell line] column. The headers must be exact:")
+    if not file:
+        st.stop()
+    df = pd.read_csv(file, index_col=0)
+    if "Cell line" not in my_dict:
+        my_dict["Cell line"] = []
+    df
 
-
-
-file = st.file_uploader("Upload the CSV file that has the columns [Compound] and [Cell line], the headers must be exact:")
-if not file:
-    st.stop()
-df = pd.read_csv(file, index_col=0)
-df
-
-dict = df.to_dict()
+    dict = df.to_dict()
 
 #fig = make_subplots(rows=1, cols=1)
 
-stat_files = st.file_uploader("drag all stat files here", accept_multiple_files=True)
+stat_files = st.file_uploader("Drag all stat files here", accept_multiple_files=True)
 if not stat_files:
     st.stop()
+
+
 show_table = st.checkbox("Show table")
 stat_file_name_dict = {}
+
 for stat_file in stat_files:
-    for read in df.index:
-        if read in stat_file.name:
-            stat_file_name_dict[stat_file.name]=[read, dict["Compound"][read],dict["Cell line"][read]]
-            title = '\t'.join(stat_file_name_dict[stat_file.name])
-            st.write(title)
-            df_stat = pd.read_csv(stat_file,delimiter='\t', skiprows=6, nrows=32, index_col=0)
-            if show_table:
-                df_stat
-                df_values = df_stat.values
+    if online:
+        for read in df.index:
+            if read in stat_file.name:
+                stat_file_name_dict[stat_file.name]=[read, dict["Compound"][read],dict["Cell line"][read]]
+                title = '\t'.join(stat_file_name_dict[stat_file.name])
+    else:
+        title = stat_file.name.replace(".stat","")
+    
+    df_stat = pd.read_csv(stat_file,delimiter='\t', skiprows=6, nrows=32, index_col=0)
+    if show_table:
+        st.write(title)
+        df_stat
+        df_values = df_stat.values
  
 
-            img = px.imshow(df_stat)
-            img.update_layout(title_text=title)
-            st.plotly_chart(img)
+    img = px.imshow(df_stat)
+    img.update_layout(title_text=title)
+    st.plotly_chart(img)
             #fig = go.Figure(data=img.data, layout=fig.layout)
             #st.plotly_chart(fig)
 
